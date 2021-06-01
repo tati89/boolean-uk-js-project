29d58f8fa1c26817c2cda2a66d517be3e5f55852
tati89 <malena.one@gmail.com>
2021-06-01 12:51:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+state = {
+  block: [
+    {
+      id: 1,
+      blockId: "A",
+      prisonerId: 1,
+    },
+    {
+      id: 2,
+      blockId: "B",
+      prisonerId: 2,
+    },
+  ],
+  prisoners: [
+    {
+      id: 1,
+      fisrtName: "Chris",
+      lastName: "Advards",
+      crimeType: "blackmail",
+      GoodBehaviour: true,
+      parole: false,
+      gangMember: true,
+    },
+    {
+      id: 2,
+      fisrtName: "Oliver",
+      lastName: "Doe",
+      crimeType: "blackmail",
+      GoodBehaviour: false,
+      parole: true,
+      gangMember: false,
+    },
+  ],
+};
