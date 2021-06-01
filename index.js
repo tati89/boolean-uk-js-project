656b01972720e1ff246f68c92f7a6290ff232de7
tati89 <malena.one@gmail.com>
2021-06-01 19:00:34 +0100

    hero section is done

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-state = {
-  block: [
-    {
-      id: 1,
-      blockId: "A",
-      prisonerId: 1,
-    },
-    {
-      id: 2,
-      blockId: "B",
-      prisonerId: 2,
-    },
-  ],
-  prisoners: [
-    {
-      id: 1,
-      fisrtName: "Chris",
-      lastName: "Advards",
-      crimeType: "blackmail",
-      GoodBehaviour: true,
-      parole: false,
-      gangMember: true,
-    },
-    {
-      id: 2,
-      fisrtName: "Oliver",
-      lastName: "Doe",
-      crimeType: "blackmail",
-      GoodBehaviour: false,
-      parole: true,
-      gangMember: false,
-    },
-  ],
-};
+let state = [];
+
+function displayPrisoner() {}
+
+function renderPrisoners() {
+  const displayContent = document.querySelector(".display-content");
+  displayContent.innerHTML = "";
+
+  const prisonerLi = document.querySelector(".prisoners-li");
+
+  prisonerLi.addEventListener("click", function () {
+    renderPrisonerList();
+  });
+}
+
+function renderPrisonerList() {
+  for (const prisoner of state.prisoners) {
+    renderPrisoner(prisoner);
+  }
+}
+
+function renderPrisoner(prisoner) {
+  const displayContent = document.querySelector(".display-content");
+  const prisonerArticle = document.createElement("article");
+  prisonerArticle.setAttribute("class", "prisoner-article");
+  displayContent.append(prisonerArticle);
+
+  const prisonerTitle = document.createElement("h2");
+  prisonerTitle.innerText = prisoner.firstName + " " + prisoner.lastName;
+  prisonerArticle.append(prisonerTitle);
+}
+function retrieveData() {
+  fetch("http://localhost:3000/prisoners")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      state.prisoners = data;
+      return data;
+    });
+}
+
+function createEventListeners() {
+  renderPrisoners();
+}
+
+retrieveData();
+createEventListeners();
