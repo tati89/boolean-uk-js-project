a464eea5bc3a072f40587ea0b8809150d780f3d2
tati89 <malena.one@gmail.com>
2021-06-03 19:37:26 +0100

    latest commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let state = [];
-
+state.crimeType = null;
 function displayPrisoner() {}
 
 function renderPrisoners() {
@@ -21,6 +21,12 @@ function renderPrisonerList() {
 
 // RENDER PRISONER
 function renderPrisoner(prisoner) {
+  if (
+    state.crimeType !== null &&
+    state.crimeType !== prisoner.crimeType.toUpperCase()
+  )
+    return;
+
   const displayContent = document.querySelector(".display-content");
   const prisonerArticle = document.createElement("article");
   prisonerArticle.setAttribute("class", "prisoner-article");
@@ -54,7 +60,7 @@ function renderPrisoner(prisoner) {
 
   const blockCell = document.createElement("span");
   blockCell.setAttribute("class", "block-sell");
-  blockCell.innerText = `Block Cell: ${prisoner.blockCell}`;
+  blockCell.innerText = `Block ${state.block[0].blockId} Cell: ${prisoner.blockCell}`;
   prisonerArticle.append(blockCell);
 
   const btnWrapper = document.createElement("div");
@@ -232,6 +238,23 @@ function editPrisonerForm(prisoner) {
   pictureInput.setAttribute("value", prisoner.picture);
   form.append(pictureInput);
 
+  //block A B C D
+  const blockDiv = document.createElement("div");
+  blockDiv.setAttribute("class", "block-cell-div");
+  form.append(blockDiv);
+
+  const blockLabel = document.createElement("label");
+  blockLabel.setAttribute("for", "block-cell-label");
+  blockLabel.innerText = "Block:";
+  blockDiv.append(blockLabel);
+
+  const blockInput = document.createElement("input");
+  blockInput.setAttribute("class", "block-cell-input");
+  blockInput.setAttribute("id", "block-cell-input");
+  blockInput.setAttribute("type", "text");
+  blockInput.setAttribute("value", "");
+  blockDiv.append(blockInput);
+
   //block Cell input + label inside div
   const blockCellDiv = document.createElement("div");
   blockCellDiv.setAttribute("class", "block-cell-div");
@@ -257,11 +280,10 @@ function editPrisonerForm(prisoner) {
   //submit button
   const submitBtn = document.createElement("button");
   submitBtn.setAttribute("class", "submit-btn");
-  submitBtn.setAttribute("type", "submit");
   submitBtn.innerText = "S A V E";
   buttonDiv.append(submitBtn);
 
-  form.addEventListener("submit", function (event) {
+  submitBtn.addEventListener("click", function (event) {
     event.preventDefault();
     updatePrisoner();
   });
@@ -281,7 +303,6 @@ function editPrisonerForm(prisoner) {
     state.prisoners[prisonerToUpdate].crimeDetails = crimeDetailsInput.value;
     state.prisoners[prisonerToUpdate].picture = pictureInput.value;
     state.prisoners[prisonerToUpdate].blockCell = blockcellInput.value;
-    console.log(state.prisoners[prisonerToUpdate]);
 
     fetch(`http://localhost:3000/prisoners/${prisoner.id}`, {
       method: "PATCH",
@@ -300,15 +321,299 @@ function editPrisonerForm(prisoner) {
         picture: state.prisoners[prisonerToUpdate].picture,
         blockCell: state.prisoners[prisonerToUpdate].blockCell,
       }),
-    });
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        fetch(`http://localhost:3000/block/${prisoner.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            blockId: blockInput.value,
+          }),
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            state.prisoners.push(data);
+          });
+      });
   }
 
   //delete button
   const deleteBtn = document.createElement("button");
   deleteBtn.setAttribute("class", "delete-btn");
-  deleteBtn.setAttribute("type", "submit");
   deleteBtn.innerText = "D I S C H A R G E";
   buttonDiv.append(deleteBtn);
+
+  deleteBtn.addEventListener("click", function () {
+    state.prisoners = state.prisoners.filter(function (inmate) {
+      return inmate.id !== prisoner.id;
+    });
+
+    fetch(`http://localhost:3000/prisoners/${prisoner.id}`, {
+      method: "DELETE",
+    });
+  });
+}
+
+function renderAdmission() {
+  const admissionBtn = document.querySelector(".admissions-li");
+
+  admissionBtn.addEventListener("click", function () {
+    renderAdmissionForm();
+  });
+}
+
+function renderAdmissionForm() {
+  const displayContent = document.querySelector(".display-content");
+  displayContent.innerHTML = "";
+
+  const formArticle = document.createElement("article");
+  formArticle.setAttribute("class", "form-article");
+  displayContent.append(formArticle);
+
+  //Form
+  const form = document.createElement("form");
+  form.setAttribute("class", "edit-form");
+  formArticle.append(form);
+
+  //Edit prisoner h1
+  const h1El = document.createElement("h1");
+  h1El.setAttribute("class", "form-title");
+  h1El.innerText = "Edit Prisoner";
+  form.append(h1El);
+
+  //First name label + input inside div
+  const firstNameDiv = document.createElement("div");
+  firstNameDiv.setAttribute("class", "first-name-div");
+  form.append(firstNameDiv);
+
+  const firstNameLabel = document.createElement("label");
+  firstNameLabel.setAttribute("for", "first-name-input");
+  firstNameLabel.innerText = "First Name:";
+  firstNameDiv.append(firstNameLabel);
+
+  const firstNameInputEl = document.createElement("input");
+  firstNameInputEl.setAttribute("class", "first-name-input");
+  firstNameInputEl.setAttribute("id", "first-name-input");
+  firstNameInputEl.setAttribute("type", "text");
+  firstNameInputEl.setAttribute("value", "");
+  firstNameDiv.append(firstNameInputEl);
+
+  //last name label + input inside div
+  const lastNameDiv = document.createElement("div");
+  lastNameDiv.setAttribute("class", "last-name-div");
+  form.append(lastNameDiv);
+
+  const lastNameLabel = document.createElement("label");
+  lastNameLabel.setAttribute("for", "last-name-input");
+  lastNameLabel.innerText = "Last Name:";
+  lastNameDiv.append(lastNameLabel);
+
+  const lastNameInputEl = document.createElement("input");
+  lastNameInputEl.setAttribute("class", "last-name-input");
+  lastNameInputEl.setAttribute("id", "last-name-input");
+  lastNameInputEl.setAttribute("type", "text");
+  lastNameInputEl.setAttribute("value", "");
+  lastNameDiv.append(lastNameInputEl);
+
+  //crime label + input inside div
+  const crimeTypeDiv = document.createElement("div");
+  crimeTypeDiv.setAttribute("class", "crime-type-div");
+  form.append(crimeTypeDiv);
+
+  const crimeType = document.createElement("label");
+  crimeType.setAttribute("for", "street-input");
+  crimeType.innerText = "Crime Type:";
+  crimeTypeDiv.append(crimeType);
+
+  const crimeTypeInput = document.createElement("input");
+  crimeTypeInput.setAttribute("class", "street-input");
+  crimeTypeInput.setAttribute("id", "street-input");
+  crimeTypeInput.setAttribute("type", "text");
+  crimeTypeInput.setAttribute("value", "");
+  crimeTypeDiv.append(crimeTypeInput);
+
+  //checkboxes
+  const divEl = document.createElement("div");
+  divEl.setAttribute("class", "checkbox-section");
+  form.append(divEl);
+
+  //Good Behaviour
+  const checkBoxInputEl = document.createElement("input");
+  checkBoxInputEl.setAttribute("class", "good-behaviour-checkbox");
+  checkBoxInputEl.setAttribute("id", "good-behaviour-checkbox");
+  checkBoxInputEl.setAttribute("type", "checkbox");
+  checkBoxInputEl.checked = false;
+  divEl.append(checkBoxInputEl);
+
+  const checkBoxLabel = document.createElement("label");
+  checkBoxLabel.setAttribute("for", "checkbox-label");
+  checkBoxLabel.innerText = "Good Behaviour";
+  divEl.append(checkBoxLabel);
+
+  //Parole
+  const paroleCheckBoxEl = document.createElement("input");
+  paroleCheckBoxEl.setAttribute("class", "parole-checkbox");
+  paroleCheckBoxEl.setAttribute("id", "parole-checkbox");
+  paroleCheckBoxEl.setAttribute("type", "checkbox");
+  paroleCheckBoxEl.checked = false;
+  divEl.append(paroleCheckBoxEl);
+
+  const paroleCheckBoxLabel = document.createElement("label");
+  paroleCheckBoxLabel.setAttribute("for", "checkbox-label");
+  paroleCheckBoxLabel.innerText = "Parole";
+  divEl.append(paroleCheckBoxLabel);
+
+  //Gang Member
+  const gangMemberCheckBox = document.createElement("input");
+  gangMemberCheckBox.setAttribute("class", "gang-member-checkbox");
+  gangMemberCheckBox.setAttribute("id", "gang-member-checkbox");
+  gangMemberCheckBox.setAttribute("type", "checkbox");
+  gangMemberCheckBox.checked = false;
+  divEl.append(gangMemberCheckBox);
+
+  const gangMemberLabel = document.createElement("label");
+  gangMemberLabel.setAttribute("for", "checkbox-label");
+  gangMemberLabel.innerText = "Gang Member";
+  divEl.append(gangMemberLabel);
+
+  //Nick name label + input inside div
+  const nickNameDiv = document.createElement("div");
+  nickNameDiv.setAttribute("class", "nick-name-div");
+  form.append(nickNameDiv);
+
+  const nickName = document.createElement("label");
+  nickName.setAttribute("for", "nick-name-input");
+  nickName.innerText = "Nick name:";
+  nickNameDiv.append(nickName);
+
+  const nickNameInput = document.createElement("input");
+  nickNameInput.setAttribute("class", "nick-name-input");
+  nickNameInput.setAttribute("id", "nick-name-input");
+  nickNameInput.setAttribute("type", "text");
+  nickNameInput.setAttribute("value", "");
+  nickNameDiv.append(nickNameInput);
+
+  //crime details input and label
+  const crimeDetails = document.createElement("label");
+  crimeDetails.setAttribute("for", "crime-details-label");
+  crimeDetails.setAttribute("class", "crime-details-label");
+  crimeDetails.innerText = "Crime details:";
+  form.append(crimeDetails);
+
+  const crimeDetailsInput = document.createElement("textarea");
+  crimeDetailsInput.setAttribute("class", "crime-details-input");
+  crimeDetailsInput.setAttribute("id", "crime-details-input");
+  crimeDetailsInput.setAttribute("type", "text");
+  form.append(crimeDetailsInput);
+
+  //picture
+  const pictureLabel = document.createElement("label");
+  pictureLabel.setAttribute("for", "picture-input");
+  pictureLabel.setAttribute("class", "picture-label");
+  pictureLabel.innerText = "Picture:";
+  form.append(pictureLabel);
+
+  const pictureInput = document.createElement("input");
+  pictureInput.setAttribute("class", "picture-input");
+  pictureInput.setAttribute("id", "picture-input");
+  pictureInput.setAttribute("type", "text");
+  pictureInput.setAttribute("value", "");
+  form.append(pictureInput);
+
+  //block A B C D
+  const blockDiv = document.createElement("div");
+  blockDiv.setAttribute("class", "block-cell-div");
+  form.append(blockDiv);
+
+  const blockLabel = document.createElement("label");
+  blockLabel.setAttribute("for", "block-cell-label");
+  blockLabel.innerText = "Block:";
+  blockDiv.append(blockLabel);
+
+  const blockInput = document.createElement("input");
+  blockInput.setAttribute("class", "block-cell-input");
+  blockInput.setAttribute("id", "block-cell-input");
+  blockInput.setAttribute("type", "text");
+  blockInput.setAttribute("value", "");
+  blockDiv.append(blockInput);
+
+  //block Cell input + label inside div
+  const blockCellDiv = document.createElement("div");
+  blockCellDiv.setAttribute("class", "block-cell-div");
+  form.append(blockCellDiv);
+
+  const blockCellLabel = document.createElement("label");
+  blockCellLabel.setAttribute("for", "block-cell-label");
+  blockCellLabel.innerText = "Cell:";
+  blockCellDiv.append(blockCellLabel);
+
+  const blockcellInput = document.createElement("input");
+  blockcellInput.setAttribute("class", "block-cell-input");
+  blockcellInput.setAttribute("id", "block-cell-input");
+  blockcellInput.setAttribute("type", "text");
+  blockcellInput.setAttribute("value", "");
+  blockCellDiv.append(blockcellInput);
+
+  //admission button
+  const btnWrapper = document.createElement("div");
+  btnWrapper.setAttribute("class", "btn-wrapper");
+  formArticle.append(btnWrapper);
+
+  const admissionBtn = document.createElement("button");
+  admissionBtn.setAttribute("class", "admission-btn");
+  admissionBtn.innerText = "A D M I S S I O N";
+  btnWrapper.append(admissionBtn);
+
+  admissionBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    fetch(`http://localhost:3000/prisoners`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: firstNameInputEl.value,
+        lastName: lastNameInputEl.value,
+        crimeType: crimeTypeInput.value,
+        goodBehaviour: checkBoxInputEl.checked,
+        parole: paroleCheckBoxEl.checked,
+        gangMember: gangMemberCheckBox.checked,
+        nickName: nickNameInput.value,
+        crimeDetails: crimeDetailsInput.value,
+        picture: pictureInput.value,
+        blockCell: blockcellInput.value,
+      }),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        fetch("http://localhost:3000/block", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            blockId: blockInput.value,
+            prisonerId: data.id,
+          }),
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            state.prisoners.push(data);
+          });
+      });
+  });
 }
 
 // RETREIVE DATA FROM LOCAL JSON SERVER
@@ -321,11 +626,21 @@ function retrieveData() {
       state.prisoners = data;
       return data;
     });
+
+  fetch("http://localhost:3000/block")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      state.block = data;
+      return data;
+    });
 }
 
 //CREATE EVENT LISTENERS FOR MAIN MENU BUTTONS
 function createEventListeners() {
   renderPrisoners();
+  renderAdmission();
 }
 
 /////////////////////////////////////////////////////////////////////
--- src/crimes.js
+++ src/crimes.js
@@ -1,5 +1,57 @@
+// MAIN PROGRAM BEGINS AT index.js
+// THIS PROVIDES FUNCTION FOR THE CRIMES MENU BUTTON
+// Author Sydney
+
+////////////////////////////////////////////////////////////////
+// ALL FUNCTIONS ARE DECLARED HERE
+
 // ADD AN EVENT LISTENER FOR THE CRIMES BUTTON
+function createButtonListener() {
+  let crimesButton = document.querySelector(".crimes-li");
+  crimesButton.addEventListener("click", function () {
+    renderCrimesButtonList();
+  });
+}
 
-// CREATE A RENDER FUNCTION TO DISPLAY CRIMES BUTTONS
+// RENDER BUTTON LIST OF CRIMES
+function renderCrimesButtonList() {
+  let contentSection = document.querySelector(".display-content");
+  contentSection.innerHTML = "";
+  state.crimeType = null;
 
-// UPDATE THE 'PRISONER' RENDER FUNCTION TO DISPLAY CRIMES ONLY
+  // CREATE A UNORDERED LIST ELEMENT
+  const crimesUl = document.createElement("ul");
+  crimesUl.setAttribute("class", "crimes-list");
+  contentSection.append(crimesUl);
+
+  // GET A LIST OF PRISONER CRIMES
+  let crimeTypes = [];
+  for (const prisoner of state.prisoners)
+    crimeTypes.push(prisoner.crimeType.toUpperCase());
+  crimeTypes = new Set(crimeTypes.sort());
+
+  // CREATE A BUTTON FOR EACH CRIME-TYPE
+  for (const crime of crimeTypes) renderCrimeButton(crime);
+}
+
+function renderCrimeButton(crime) {
+  let crimesUl = document.querySelector(".crimes-list");
+
+  // CREATE A 'CRIME' LIST ITEM BUTTON
+  const crimesLi = document.createElement("li");
+  crimesLi.setAttribute("class", "crimes-list-item");
+  crimesLi.innerText = crime;
+  crimesUl.append(crimesLi);
+
+  // CREATE AN EVENT LISTENER TO DISPLAY PRISONERS
+  crimesLi.addEventListener("click", function () {
+    state.crimeType = crime;
+    renderPrisonerList();
+  });
+}
+
+////////////////////////////////////////////////////////////////
+// MAIN PROGRAM STARTS IN index.js
+// CRIME BUTTON FUNCTIONS BEGIN HERE
+
+createButtonListener();
